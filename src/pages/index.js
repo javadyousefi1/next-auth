ca2a70b148b4394c7f83b30094ba612a278bf39d
Javad Yousefi <javadyousefi9000@gmail.com>
2023-11-23 21:30:38 +0330

    add liara to app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,13 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/auth");
+  }, []);
+
   return (
     <p className="m-4 font-bold text-center text-blue-500">تست فونت کلمه</p>
   );
